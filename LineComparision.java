d6fb73d2894934baac115372e770d2f9d91df465
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-08 14:33:47 +0530

    Check Equality

--- LineComparision.java
+++ LineComparision.java
@@ -1,11 +1,67 @@
+import java.util.*;
+
 public class LineComparision {
 
         public static void main(String args[]) {
 
-                System.out.println("Welcome to Line Comparision Program");
-		int x1=4,y1=3,x2=6,y2=9;
-		double distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1) *(y2-y1)) ;
-		//System.out.println(distance);
-		System.out.println("Distance between "+"("+x1+","+y1+"),"+"("+x2+","+y2+") is "+distance);
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Enter the value of X1 :- ");
+
+                int X1=scan.nextInt();
+
+                System.out.print("Enter the value of Y1 :- ");
+
+                int Y1=scan.nextInt();
+
+                System.out.print("Enter the value of X2 :- ");
+
+                int X2=scan.nextInt();
+
+                System.out.print("Enter the value of Y2 :- ");
+
+                int Y2=scan.nextInt();
+
+		System.out.print("Enter the value of X3 :- ");
+
+                int X3=scan.nextInt();
+
+                System.out.print("Enter the value of Y3 :- ");
+
+                int Y3=scan.nextInt();
+
+                System.out.print("Enter the value of X4 :- ");
+
+                int X4=scan.nextInt();
+
+                System.out.print("Enter the value of Y4 :- ");
+
+                int Y4=scan.nextInt();
+
+		double distance1 = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1) *(Y2-Y1)) ;
+
+                System.out.println("Distance between "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+distance1);
+
+		double distance2 = Math.sqrt((X4-X3)*(X4-X3) + (Y4-Y3) *(Y4-Y3)) ;
+
+                System.out.println("Distance between "+"("+X3+","+Y3+"),"+"("+X4+","+Y4+") is "+distance2);
+
+		if ( distance1 == distance2 ) {
+
+		 System.out.println("These two lengths are preferable");
+
+		}
+
+		else {
+
+		System.out.println("These two lengths are not preferable");
+
+		}
+
+
+
+
+
+
 	}
 }
